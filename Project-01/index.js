c0c649b9751a41ee3980bff7089286254e5cc9ac
dinesh <dineshlalam15@gmail.com>
2024-06-22 01:05:35 +0530

    POST & DELETE

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,22 +1,56 @@
 const express = require('express')
+const fs = require('fs')
 const users = require('./MOCK_DATA.json')
 const port = 8000
 const app = express()
 
+// Middleware - Plugin
+app.use(express.urlencoded({extended: false}))
+
 app.route("/users/:id").get((req, res) => {
     const id = Number(req.params.id)
-    const user = users.find(element => element.id === id)
-    return res.json(user)
+    if(users.find(element => element.id === id)){
+        const user = users[id-1]
+        return res.json(user)
+    } else{
+        return res.send(`User with this ${id} doesn't exist`)
+    }
 }).put((req,res) => {
-    return res.send('Status: Pending')
+    return res.json({Status: "Pending"})
 }).patch((req,res) => {
-    return res.send('Status: Pending')
+    return res.json({Status: "Pending"})
 }).delete((req,res) => {
-    return res.send('Status: Pending')
+    const id = Number(req.params.id)
+    if(users.find(element => element.id === id)){
+        const deletedUser = users[id-1]
+        users.splice(id-1, id-1);
+        return res.send(`Status: ${deletedUser} Succesfully deleted`)
+    } else{
+        return res.send(`User ${id} doesn't exist. Unable to perform the action.`)
+    }
 })
 
 app.get("/users", (req,res) => {
     return res.json(users)
+})
+app.post("/newUser", (req, res) => {
+    const newUserID = users[users.length - 1].id + 1;
+    const body = req.body
+    users.push({
+        id: newUserID,
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        job_title: body.job_title
+    })
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+        if(err){
+            return res.send("Error", err)
+        } else{
+            return res.json({Status: "Finished", id: newUserID})
+        }
+    })
 })
 
 app.listen(port, () => console.log(`Server running on port`, port))
