9146656e609455db5e778b71bda698de21a04567
dinesh <dineshlalam15@gmail.com>
2024-06-21 18:16:51 +0530

    app.route() is added

--- Project-01/index.js
+++ Project-01/index.js
@@ -3,13 +3,20 @@ const users = require('./MOCK_DATA.json')
 const port = 8000
 const app = express()
 
+app.route("/users/:id").get((req, res) => {
+    const id = Number(req.params.id)
+    const user = users.find(element => element.id === id)
+    return res.json(user)
+}).put((req,res) => {
+    return res.send('Status: Pending')
+}).patch((req,res) => {
+    return res.send('Status: Pending')
+}).delete((req,res) => {
+    return res.send('Status: Pending')
+})
+
 app.get("/users", (req,res) => {
     return res.json(users)
 })
 
-app.get("/users/:id", (req,res) => {
-    const id = Number(req.params.id)
-    const user = users.find(element => element.id === id)
-    return res.json(user)
-})
 app.listen(port, () => console.log(`Server running on port`, port))
