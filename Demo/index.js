b34b5d4f4d3e4d6a17c249d550d08a1f564b3f86
dinesh <dineshlalam15@gmail.com>
2024-07-09 22:14:54 +0530

    production level PORT application

--- Demo/index.js
+++ Demo/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 4000
+app.get('/', (req, res) => {
+    res.send('Hello Kavya!')
+})
+app.listen(port, () => {
+    console.log(`Demo started on PORT ${port}`)
+})
