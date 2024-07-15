7f2e23f23c8297ab22e069a338515ce91ef6cc3a
dinesh <dineshlalam15@gmail.com>
2024-07-15 15:09:11 +0530

    Professional way of establishing database connection.

--- Demo/src/index.js
+++ Demo/src/index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config({ path: '../.env' });
-const app = express()
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const port = process.env.PORT || 4000 
-mongoose.connect(process.env.MONGODB_URI)
-app.listen(port, () => {
-    console.log(`PORT ${port}`)
-})
+const port = process.env.PORT || 3000;
+const mongoDB = process.env.MONGODB_URI;
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(mongoDB);
+        console.log("Connected to the Database");
+        console.log("Connection String:", mongoDB);
+        app.listen(port, () => {
+            console.log(`Application running on PORT ${port}`);
+        });
+    } catch (error) {
+        console.error("Error:", error);
+        throw error;
+    }
+})();
