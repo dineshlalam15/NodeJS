09dd3e718ad2f9263b93f7a1b1e823dbb6c9b609
dinesh <dineshlalam15@gmail.com>
2024-07-16 15:40:56 +0530

    mongo folder code (index.js) updated.

--- mongo/index.js
+++ mongo/index.js
@@ -1,143 +1,146 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
 
-const port = 8000;
-const app = express();
-
+const app = express()
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: true }));
 
-const personSchema = new mongoose.Schema({
-    FirstName: { 
-        type: String, 
-        required: true 
-    },
-    LastName: { 
-        type: String, 
-        required: true 
-    },
-    Gender: { 
-        type: String, 
-        required: true 
-    },
-    Place: { 
-        type: String, 
-        required: true 
-    },
-    Industry: { 
-        type: String, 
-        required: true 
-    },
-    Vehicle: { 
-        type: String, 
-        required: true 
-    }
-})
-const personModel = mongoose.model('personModel', personSchema, 'people');
-
+const port = process.env.PORT || 8000
 const uri = process.env.MONGODB_URI;
-if (!uri) {
-    console.log('MongoDB URI is not defined in the environment variables');
-    process.exit(1);
-}
-mongoose.connect(uri, { 
+
+mongoose.connect(uri, {
     serverSelectionTimeoutMS: 5000
 }).then(() => {
-    console.log('Connected to MongoDB Atlas');
+    console.log(`Database Connection Successful`);
     app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
+        console.log(`Application started on PORT ${port}`);
     });
-}).catch((err) => {
-    console.error('Error connecting to MongoDB Atlas:', err);
+}).catch((error) => {
+    console.error("Error: ", error);
     process.exit(1);
 });
 
+const personSchema = new mongoose.Schema(
+    {
+        id: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        first_name: {
+            type: String,
+            required: true
+        },
+        last_name: {
+            type: String,
+            required: true
+        },
+        gender: {
+            type: String,
+            required: true
+        },
+        place: {
+            type: String,
+            required: true
+        },
+        vehicle: {
+            type: String,
+            required: true
+        },
+    }, { timestamps: true }
+)
+const Person = mongoose.model('Person', personSchema, 'Person_Details')
+
 app.get('/persons', async (req, res) => {
     try {
-        const persons = await personModel.find({})
-        return res.json(persons)
+        const persons = await Person.find({})
+        return res.status(200).json(persons)
     } catch (err) {
         console.error('Error retrieving persons:', err);
-        return res.status(500).json({ error: 'Error occurred while retrieving persons' });
+        return res.status(500).json({ error: 'An error occured while retrieving persons details' });
     }
 })
-app.post('/newuser', async (req, res) => {
+
+app.post('/newperson', async (req, res) => {
     const body = req.body
-    console.log('Request body:', req.body);
-    const newPerson = new personModel({
-        FirstName: body.FirstName,
-        LastName: body.LastName,
-        Gender: body.Gender,
-        Place: body.Place,
-        Industry: body.Industry,
-        Vehicle: body.Vehicle
-    })
     try{
+        const lastUser = await Person.findOne().sort({ id: -1 });
+        const newPersonId = lastUser ? lastUser.id + 1 : 1
+        const newPerson = new Person(
+            {
+                id: newPersonId,
+                first_name: body.first_name,
+                last_name: body.last_name,
+                gender: body.gender,
+                place: body.place,
+                vehicle: body.vehicle
+            }
+        )
         const savedPerson = await newPerson.save()
         return res.status(201).json(savedPerson)
     } catch (err) {
         console.error('Error savong new person:', err)
-        return res.status(500).json({error: 'Error occured while saving new person'})
+        return res.status(500).json({error: 'An error occured while saving new person'})
     }
 })
 
-app.route('/person/:name')
+app.route('/person/:id')
 .get(async (req, res) => {
-    const findName = req.params.name
-    try{
-        const findPerson = await personModel.findOne({FirstName: findName})
-        if(findPerson){
-            return res.status(201).json(findPerson)
-        } else{
-            return res.status(404).json({Error: 'Person with that name does not exist'})
+    const findId = req.params.id;
+    try {
+        const findPerson = await Person.findOne({ id: findId });
+        console.log(findPerson);
+        if (findPerson) {
+            return res.status(200).json(findPerson);
+        } else {
+            return res.status(404).json({ error: `Person with id ${findId} does not exist` });
         }
-    } catch{
-        console.error('Error deleting person:', err);
-        return res.status(500).json({ error: 'Error occurred while fetching the person details' });
+    } catch (err) {
+        console.error('Error fetching person details:', err);
+        return res.status(500).json({ error: 'An error occurred while fetching the person details' });
     }
 })
 .delete(async (req, res) => {
-    const firstName = req.params.name
+    const findId = req.params.id
     try {
-        const deletedPerson = await personModel.findOneAndDelete({ FirstName: firstName });
+        const deletedPerson = await Person.findOneAndDelete({ id: findId });
         if (!deletedPerson) {
             return res.status(404).json({ error: 'Person not found' });
         }
         console.log(deletedPerson)
-        return res.status(200).json({ message: 'Person deleted successfully', person: deletedPerson });
+        return res.status(200).json({ message: 'Person Deleted successfully', person: deletedPerson });
     } catch (err) {
         console.error('Error deleting person:', err);
-        return res.status(500).json({ error: 'Error occurred while deleting person' });
+        return res.status(500).json({ error: 'An error occurred while deleting person details' });
     }
 })
 .patch(async (req, res) => {
-    const findName = req.params.name;
+    const findId = req.params.id
     const body = req.body;
     try {
-        const findPerson = await personModel.findOne({FirstName: findName})
+        const findPerson = await Person.findOne({id: findId})
         if(!findPerson){
-            return res.status(404).json({"Message": `User with first name ${findName} doesn't exist`})
+            return res.status(404).json({"Message": `User with id ${findId} doesn't exist`})
         }
-        const updatedPerson = await personModel.findOneAndUpdate(
-            { FirstName: findName },
+        const updatedPerson = await Person.findOneAndUpdate({ id: findId },
             {
-                FirstName: body.FirstName ? body.FirstName : findPerson.FirstName,
-                LastName: body.LastName ? body.LastName : findPerson.LastName,
-                Gender: body.Gender ? body.Gender : findPerson.Gender,
-                Place: body.Place ? body.Place : findPerson.Place,
-                Industry: body.Industry ? body.Industry : findPerson.Industry,
-                Vehicle: body.Vehicle ? body.Vehicle : findPerson.Vehicle
+                first_name: body.first_name ? body.first_name : findPerson.first_name,
+                last_name: body.last_name ? body.last_name : findPerson.last_name,
+                gender: body.gender ? body.gender : findPerson.gender,
+                place: body.place ? body.place : findPerson.place,
+                vehicle: body.vehicle ? body.vehicle : findPerson.vehicle
             },
             { new: true }
         );
         if (updatedPerson) {
             return res.status(200).json(updatedPerson);
         } else {
-            return res.status(404).json({ error: 'Person not found' });
+            return res.status(404).json({"Message": `User with id ${findId} doesn't exist`})
         }
     } catch (err) {
         console.error('Error updating person:', err);
-        return res.status(500).json({ error: 'Error occurred while editing the person details' });
+        return res.status(500).json({ error: 'An error occurred while editing the person details' });
     }
 })
