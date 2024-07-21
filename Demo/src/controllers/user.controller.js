50064c04ecfd3b0ca1980821c8c6b9a053b8b05c
dinesh <dineshlalam15@gmail.com>
2024-07-21 09:11:10 +0530

    Validation criterias updated for userName, fullName, email, password

--- Demo/src/controllers/user.controller.js
+++ Demo/src/controllers/user.controller.js
@@ -3,19 +3,27 @@ import { APIError } from "../utils/APIError.js";
 import { APIResponse } from "../utils/APIResponse.js";
 import User from '../models/user.model.js'
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import { isEmpty, validateEmail, validatePassword, validateUsername } from "../validation.js";
 
 const registerUser = asyncHandler( async (req, res) => {
     const {fullName, email, userName, password} = req.body
     console.log(`${fullName}, ${email}, ${userName}, ${password}`);
     const details = [fullName, email, userName, password];
-    details.forEach(detail => {
-      if (detail === "") {
-        throw new APIError(400, `${detail} is required`);
-      }
-    });
-    if(!email.includes('@')){
+    details.forEach(element => {
+        if(isEmpty(element)){
+            throw new APIError(400, `${element} is required`)
+        }
+    })
+    if(!validateEmail(email)){
         throw new APIError(400, `Invalid Email Id`)
     }
+    if(!validateUsername(userName)){
+        throw new APIError(400, `${userName} not available`)
+    }
+    if(!validatePassword(password)){
+        throw new APIError(400, `Password seem's not safe. Try it again`)
+    }
+
     const existedUserName = await User.findOne({userName: userName})
     const existedEmail = await User.findOne({email: email})
     if(existedUserName){
@@ -26,11 +34,14 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.file?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if(!avatarLocalPath){
         throw new APIError(400, `Avatar file is required`)
     }
-    
+
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
     if(!avatar){
@@ -43,7 +54,7 @@ const registerUser = asyncHandler( async (req, res) => {
         email: email,
         password: password,
         avatar: avatar.url,
-        coverImage: coverImage ? coverImage.url : "",
+        coverImage: coverImage.url || "",
     })
     const createdUser = await User.findById(newUser._id).select(
         "-password -refreshToken"
